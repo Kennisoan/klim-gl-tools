f7c7c842a0bf632adea14853e3e458bdebba5c32
Klim Korovkin <klimkorovkin@yandex.ru>
2024-03-12 09:13:08 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+		name='klim-glut-tools',
+		version='0.1',
+		packages=['klim_glut_tools'],
+		description='A collection of tools for OpenGL projects',
+		author='Klim Korovkin',
+		author_email='klimkorovkin@yandex.ru',
+		url='https://github.com/Kennisoan/klim-glut-tools',
+		install_requires=[
+				'PyOpenGL'
+		],
+)
